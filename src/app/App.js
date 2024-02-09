e168a8394e26d3b56060c3496c9b14e658d5e223
Sergey Ezdakov <esdakov@mail.ru>
2024-02-10 00:24:35 +0300

    undo a commit to main

--- src/app/App.js
+++ src/app/App.js
@@ -7,8 +7,6 @@ import Main from "./layouts/main";
 import NavBar from "./components/ui/navBar";
 import Basket from "./layouts/basket";
 import Admin from "./layouts/admin";
-import AdminFormAdd from "./components/ui/adminFormAdd";
-import AdminFormChange from "./components/ui/adminFormChange";
 // import AdminPageList from "./components/page/adminPageList/adminPageList";
 
 const App = () => {
@@ -19,8 +17,6 @@ const App = () => {
                 <Route path="/products/:prodId?" component={Products} />
                 <Route path="/login/:type?" component={Login} />
                 <Route path="/admin/:prodId?" component={Admin} />
-                <Route path="/adminFormAdd" component={AdminFormAdd} />
-                <Route path="/adminFormChange" component={AdminFormChange} />
                 {/* <Route path="/adminPagelist" component={AdminPageList} /> */}
                 <Route path="/basket" component={Basket} />
                 <Route path="/" exact component={Main} />
